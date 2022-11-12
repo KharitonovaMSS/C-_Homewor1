f8c3b94b8398d5aa256c94681c8b730d49f74449
Maria <mineeva_maria@mail.ru>
2022-11-12 18:32:44 +0300

    Найдено большее из трех чисел

---
+++ Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine (+ a > + b);
+}
+else
+{
+ Console.WriteLine (+ b > + a);   
+}
+if (a > c)
+{
+    Console.WriteLine(+ a > + c);
+}
+else
+{
+    Console.WriteLine(+ c > + a);
+}
+if (b > c)
+{
+    Console.WriteLine(+ b > + c);
+}
+else
+{
+    Console.WriteLine(+ c > + b);
+}
